41d61bf08ba2387438702f47ef548c8fdc5aed32
JesusVillanuev <jesus.villanueva235078@potros.itson.edu.mx>
2025-02-18 20:47:55 -0700

    Terminado hasta el video

---
+++ app/src/main/java/jesus/villanueva/mydigimind/Recordatorio.kt
@@ -0,0 +1,6 @@
+package jesus.villanueva.mydigimind
+import java.io.Serializable
+
+class Recordatorio (var dias:String,
+                    var tiempo:String,
+                    var nombre:String): Serializable

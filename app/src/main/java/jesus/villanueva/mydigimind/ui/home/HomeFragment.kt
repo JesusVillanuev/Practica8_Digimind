a8197aabb9f326a93e5a6f145622da857055c4ca
JesusVillanuev <jesus.villanueva235078@potros.itson.edu.mx>
2025-02-18 21:20:03 -0700

    Terminado

--- app/src/main/java/jesus/villanueva/mydigimind/ui/home/HomeFragment.kt
+++ app/src/main/java/jesus/villanueva/mydigimind/ui/home/HomeFragment.kt
@@ -1,42 +1,82 @@
 package jesus.villanueva.mydigimind.ui.home
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.GridView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import jesus.villanueva.mydigimind.Carrito
+import jesus.villanueva.mydigimind.R
 import jesus.villanueva.mydigimind.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private lateinit var homeViewModel: HomeViewModel
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    companion object {
+        val carrito: Carrito = Carrito()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
+        homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        homeViewModel.text.observe(viewLifecycleOwner, { })
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+       var gridview: GridView = root.findViewById<GridView>(R.id.gridView)
+
+       var adaptador: RecordatorioAdapter = RecordatorioAdapter(container!!.context, carrito)
+       gridview.adapter = adaptador
+
         return root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+}
+class RecordatorioAdapter: BaseAdapter {
+    var carrito = Carrito()
+    var context: Context? = null
+
+    constructor(context: Context, carrito: Carrito) {
+        this.context = context
+        this.carrito = carrito
+    }
+
+    override fun getCount(): Int {
+        return carrito.recordatorios.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return carrito.recordatorios[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var recordatorio = carrito.recordatorios[position]
+        var inflator =
+            context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        var vista = inflator.inflate(R.layout.recordatorio, null)
+
+        val txtNombreRecordatorio = vista.findViewById<TextView>(R.id.txtNombreRecordatorio)
+        val txtDiasRecordatorio = vista.findViewById<TextView>(R.id.txtDiasRecordatorio)
+        val txtTiempoRecordatorio = vista.findViewById<TextView>(R.id.txtTiempoRecordatorio)
+
+        txtNombreRecordatorio.setText(recordatorio.nombre)
+        txtDiasRecordatorio.setText(recordatorio.dias)
+        txtTiempoRecordatorio.setText(recordatorio.tiempo)
+
+        return vista
     }
 }
